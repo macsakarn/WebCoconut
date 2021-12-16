874204f6eeb59ace631d43b721537ce7792ba1f5
macsakarn <macsakarn@gmail.com>
2021-12-16 20:42:46 +0700

    .

--- cocotree/pages/test.vue
+++ cocotree/pages/test.vue
@@ -1,14 +1,11 @@
 <template>
-  <input
-    class="w-full border-2 p-3 rounded outline-none"
-    type="text"
-    :class="{
-      'border-red-500 focus:border-red-500': $v.userLogin.$error,
-      'focus:border-purple-500 border-gray-200 ': !$v.userLogin.$error,
-    }"
-    placeholder="E-mail"
-    v-model="$v.userLogin.$model"
-  />
+  <div class="">
+    <div class="island">
+      <div class="wave wave1"></div>
+      <div class="wave wave2"></div>
+      <div class="wave wave3"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,4 +26,49 @@ export default {
 </script>
 
 <style>
+
+.bigsand {
+  transform: scale(2, 1);
+  bottom: 0;
+}
+.sand {
+  fill: #e4c9a0;
+}
+
+.wave {
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+  min-height: 150px;
+  width: 100%;
+ 
+  left: 0;
+  animation: animation-pv6mzq 50s linear infinite;
+}
+.wave3 {
+  top: 600px;
+  background-image: url('~assets/wave3.png');
+  z-index: 5;
+}
+.wave2 {
+  top: 500px;
+  background-image: url('~assets/wave2.png');
+  z-index: 5;
+}
+.wave1 {
+  top: 400px;
+  background-image: url('~assets/wave1.png');
+  z-index: 5;
+}
+
+@keyframes animation-pv6mzq {
+  0% {
+    -webkit-background-position: 0 0;
+    background-position: 0 0;
+  }
+  100% {
+    -webkit-background-position: 972px 0;
+    background-position: 972px 0;
+  }
+}
 </style>
